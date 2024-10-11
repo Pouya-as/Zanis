ec560d5a6311c50bc065bdd0a20c25f456e35676
Pouya <137420821+Pouya-as@users.noreply.github.com>
2024-10-11 14:12:37 +0330

    Header completed
    add brand title and button
    rotate button (click)
    change style brand (click)

--- src/default-script.js
+++ src/default-script.js
@@ -4,3 +4,15 @@
 const date = new Date;
 document.getElementById("date").textContent =
 date.toLocaleString("fa-IR", {dateStyle: "full",});
+
+
+// Header Button 
+const toggle = document.getElementById('nav-toggle-btn')
+toggle.addEventListener('click', function() {
+
+    // Header Button rotate
+    document.body.classList.toggle('btn-rotate')
+
+    // header-title style in h1 
+    document.body.classList.toggle('header-brand');
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'background' : '#edece7',
-        'hd-menu' : '#f65d35',
-        'menu-button' : '#f7714d',
+        'header-btn' : '#f65d35',
+        'hd-button' : '#f7714d',
         'menu' : '#433d3c',
         'navbar': '#363130',
       },
